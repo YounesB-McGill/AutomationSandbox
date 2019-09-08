ef5ce8f3da13dd7c003911b388e9caef82ecf604
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2019-09-08 16:48:57 -0400

    Add failing test

--- test/ca/mcgill/cleancode/AppTest.java
+++ test/ca/mcgill/cleancode/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void appHasAGreeting() {fail();
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
